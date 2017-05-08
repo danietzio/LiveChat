7082f2d467d6413ccf92a81b148bd4189423b54c
danietzio <dani.nasiri@gmail.com>
2017-05-08 14:42:45 +0430

    Creating main structure of project

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// render React components inside of the root div
+ReactDOM.render(
+    <h1>Live Chat Project Started!!</h1>,
+    document.getElementById("root"),
+    function() {
+      console.log("root Compnonet rendered!!!");
+    }
+);
