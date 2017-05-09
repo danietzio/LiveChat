8a5864b0512e7fe13d12ba5687efe7ce2ca699ab
danietzio <dani.nasiri@gmail.com>
2017-05-09 19:07:07 +0430

    adding agent panel to application

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,18 +8,19 @@ import gutil from 'gulp-util';
 
 watchify.args.debug = true;
 
-const sync = browserSync.create();
+const sync = browserSync.create('server1');
+const sync2 = browserSync.create('server2');
 
 // Input file.
 watchify.args.debug = true;
 
 // bundler function
 function bundle() {
-    return gulp.src('src/app.js')
+    return gulp.src('src/client/app.js')
         .pipe(webpack(require('./webpack.config.js')))
         .pipe(rename("bundle.js"))
         .on('error', gutil.log)
-        .pipe(gulp.dest('public/assets/js'));
+        .pipe(gulp.dest('public/client/assets/js'));
 }
 
 gulp.task('default', ['transpile']);
@@ -27,7 +28,7 @@ gulp.task('default', ['transpile']);
 gulp.task('transpile', ['lint'], () => bundle());
 
 gulp.task('lint', () => {
-    return gulp.src(['src/**/*.js', 'gulpfile.babel.js'])
+    return gulp.src(['src/client/**/*.js', 'gulpfile.babel.js'])
         .pipe(eslint())
         .pipe(eslint.format())
 });
@@ -36,16 +37,25 @@ gulp.task('serve', ['transpile'], () => {
     delete process.env.BROWSER;
 
     sync.init({
-        server: 'public',
+        server: './public/client',
         port: process.env.PORT || 8000,
         host: process.env.IP || 'localhost'
     });
+
+    sync2.init({
+        server : './public/agent',
+        proxy : 'http://localhost:8080'
+    });
+
 });
 
-gulp.task('js-watch', ['transpile'], () => sync.reload());
+gulp.task('js-watch', ['transpile'], () => { sync.reload() });
 
 gulp.task('watch', ['serve'], () => {
-    gulp.watch('src/**/*', ['js-watch'])
-    gulp.watch('public/assets/styles/style.css', sync.reload)
-    gulp.watch('public/index.html', sync.reload)
+
+    gulp.watch('src/client/**/*', ['js-watch'])
+    gulp.watch('public/client/assets/styles/style.css', sync.reload)
+    gulp.watch('public/client/index.html', sync.reload)
+
+
 });
---
+++ src/agent/server/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var express = require('express');
+var app = express.createServer();
+
+
+app.listen(8080, () => console.log("Server Listening to port 8080"));
