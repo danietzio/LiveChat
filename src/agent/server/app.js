fc9f2b3ede874a1f03c651823d8b1f514c48b30a
danietzio <dani.nasiri@gmail.com>
2017-05-14 18:02:11 +0430

    client side panel basic chat application ( part 1 )

--- src/agent/server/app.js
+++ src/agent/server/app.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var app = express.createServer();
 var path = require('path');
+var io = require('socket.io');
 
 app.use(express.static(path.join(__dirname, '../../../public/agent')));
 
@@ -13,3 +14,25 @@ app.get('/', (req,res, next) => {
 });
 
 app.listen(8080, () => console.log("Server Listening to port 8080"));
+
+
+io.on('connection', function (socket) {
+  console.log('a user connected');
+
+  socket.on('disconnect', function () {
+      console.log("User disconnected");
+  });
+
+  socket.on('client message', function(val) {
+      console.log('client Message Recived : ', val.name, val.msg);
+
+      // send msg to specific user
+      io.sockets.connected[socket.id].emit('agentMessage',
+          {
+            'name' : 'Agent',
+            'msg' : 'Hi sir how are you? how can i help you??',
+            'date' : new Date()
+          });
+  });
+
+});
--- src/client/layouts/layout.js
+++ src/client/layouts/layout.js
@@ -8,6 +8,12 @@ export default class Layout extends React.Component {
     constructor() {
       super();
 
+      this.state = {
+        messages : []
+      };
+
+      // binding this class to functions
+      this.messagesTempate = this.messagesTempate.bind(this);
     }
 
     // before that component rendered
@@ -21,20 +27,15 @@ export default class Layout extends React.Component {
             <div className="appContainer">
               <div  className="chatAppContainer fluid-container">
                 <div className="row chatBox">
-                    <header>
-                      <p>Live Chat application</p>
-                    </header>
-                    <div className="quotas well">
-                      <span>
-                        <span>Dani</span>
-                        <span>Hi Customer</span>
-                      </span>
-                    </div>
+                  <header>
+                    <p>Live Chat application</p>
+                  </header>
+                  { this.messagesTempate() }
                 </div>
                 <div className="row sendBox">
-                    <form action="">
-                      <input type="text" placeholder="Type Here...."/>
-                    </form>
+                  <form action="">
+                    <input type="text" placeholder="Type Here...."/>
+                  </form>
                 </div>
               </div>
             </div>
@@ -44,7 +45,32 @@ export default class Layout extends React.Component {
 
     // after that component rendered
     componentDidMount() {
+        $(".sendBox > form").on('submit', (e) => {
+          e.preventDefault();
 
+          // client anwser to agent
+          let clientAnwser = {
+            name : 'client',
+            msg : 'I have a realy fucking problem!!!',
+            date : new Date()
+          }
+
+          
+        });
     }
 
+    // function for rendering messages in template
+    messagesTempate() {
+      return this.state.messages.map((msg, index) => {
+        return (
+          <div className="quotas well">
+            <span>
+              <span> { msg.name } </span>
+              <span> { msg.msg } </span>
+              <span> { msg.date.toString() } </span>
+            </span>
+          </div>
+        )
+      })
+    }
 }
